419ae810dc4cfcc359c3e73ec2dc71eae0491d59
Timo Kramer <fw-git@timokramer.de>
2020-06-23 20:03:10 +0200

    test release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timokramer/datahike "0.5.1-SNAPSHOT"
+(defproject timokramer/datahike "0.5.1"
   :description "A durable datalog implementation adaptable for distribution."
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
