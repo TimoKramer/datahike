a01146fd11e76f66ad47c8f82da1addaedfca689
Timo Kramer <info@lambdaforge.io>
2021-11-29 23:26:04 +0100

    fixup! WIP

--- build.clj
+++ build.clj
@@ -1,9 +1,11 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
-            [deps-deploy.deps-deploy :as dd]))
+            [deps-deploy.deps-deploy :as dd]
+            [borkdude.gh-release-artifact :as gh]))
 
 (def lib 'timokramer/datahike)
 (def version (format "0.4.%s" (b/git-count-revs nil)))
+(def current-commit (gh/current-commit))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
@@ -30,37 +32,29 @@
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
 
-(defn tag [_]
-  (b/git-process {:git-args ["config" "--local" "user.email" "info@lambdaforge.io"]})
-  (b/git-process {:git-args ["config" "--local" "user.name" "\"CircleCI Release-Pipeline\""]})
-  (b/git-process {:git-args ["config" "--local" "credential.helper" '"!f()" "{" "sleep" "1;" "echo" "\"username=${GITHUB_USER}\";" "echo" "\"password=${GITHUB_TOKEN}\";" "};" "f'"]})
-  (println (b/process {:command-args ["printenv"]
-                       :out :capture}))
-  (println (b/git-process {:git-args ["config" "--list" "--show-origin"]}))
-  (b/git-process {:git-args ["tag" "-a" (format "v%s" version) "-m" "Released by CircleCI Pipeline"]})
-  (b/git-process {:git-args ["remote" "add" "writeaccess" (format "https://github.com/%s.git" lib)]})
-  (b/git-process {:git-args ["push" "--tags" "writeaccess"]}))
-
-(comment
-  (str lib))
-
 (defn deploy [_]
   (println "Don't forget to set CLOJARS_USERNAME and CLOJARS_PASSWORD env vars.")
   (dd/deploy {:installer :remote :artifact jar-file
               :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
 
+(defn release [_]
+  (-> (gh/overwrite-asset {:org (namespace lib)
+                           :repo (name lib)
+                           :tag version
+                           :commit current-commit
+                           :file jar-file
+                           :content-type "application/java-archive"})
+      :url
+      println))
+
 (defn install [_]
+  (clean nil)
+  (jar nil)
   (b/install {:basis (b/create-basis {})
               :lib lib
               :version version
               :jar-file jar-file
               :class-dir class-dir}))
-
-(defn ci [_]
-  (clean nil)
-  (jar nil)
-  (tag nil)
-  (deploy nil))
 
 (comment
   (b/pom-path {:lib lib :class-dir class-dir})
@@ -69,4 +63,10 @@
   (jar nil)
   (tag nil)
   (deploy nil)
-  (install nil))
+  (release nil)
+  (install nil)
+
+  (name lib)
+  (namespace lib)
+  (require '[babashka.fs :as fs])
+  (fs/file-name (format "target/datahike-%s.jar" version)))
